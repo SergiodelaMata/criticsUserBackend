866edbcd203463abf027529c7f9c2fc65d7e1a81
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-02 08:51:11 +0100

    :sparkles: Created the application structure and connection with its database and to be used be other microservices.

---
+++ src/main/java/com/practices/sergiodelamata/criticsUserBackend/CriticsUserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.criticsUserBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticsUserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticsUserBackendApplication.class, args);
+	}
+
+}
